f55d2ce99507ebfecc1f0513e401ffb0b42f2a5e
aubert-gloire <a.bihibindi1@alustudent.com>
2024-02-27 22:51:33 -0800

    1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -5,11 +5,4 @@ def element_at(my_list, idx):
     elif idx > len(my_list) - 1:
         return "None"
     else:
-        return my_list[idx]#!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0:
-        return "None"
-    elif idx > len(my_list) - 1:
-        return "None"
-    else:
         return my_list[idx]
